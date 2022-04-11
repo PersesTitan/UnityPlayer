a53736e39f97f7178500dd6aa42ebc09f825bccf
PersesTitan <torampersestitan@gmail.com>
2022-04-11 18:13:21 +0900

    오브잭트 반복 좌우이동

---
+++ left_right_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    Vector3 pos; //현재위치
+    float delta = 2.0f; // 좌(우)로 이동가능한 (x)최대값
+    float speed = 3.0f; // 이동속도
+
+    void Start () {
+        pos = transform.position;
+    }
+
+    void Update () {
+        Vector3 v = pos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
