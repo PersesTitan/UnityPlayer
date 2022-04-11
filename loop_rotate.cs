032ab749e70148f4f65fc3ea9e523d8055beac2d
PersesTitan <torampersestitan@gmail.com>
2022-04-11 18:10:44 +0900

    물체 회전 반복

---
+++ loop_rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour {
+
+    public float degreePerSecond;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.Rotate(Vector3.up * Time.deltaTime * degreePerSecond);
+    }
+}
